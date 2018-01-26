cb4858d472c7f0fab80a11ebfd0d1e7a71a4155f
Ngan Pham <ngan@users.noreply.github.com>
2018-01-26 15:37:00 -0800

    Add ERC20 functions to interface

--- contracts/InkProtocolInterface.sol
+++ contracts/InkProtocolInterface.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.11;
 
 interface InkProtocolInterface {
+  /* Protocol */
   function link(address _to) external;
   function createTransaction(address _seller, uint256 _amount, bytes32 _metadata, address _policy, address _mediator) external;
   function createTransactionWithOwner(address _seller, uint256 _amount, bytes32 _metadata, address _policy, address _mediator, address _owner) external;
@@ -17,4 +18,11 @@ interface InkProtocolInterface {
   function confirmTransactionByMediator(uint256 _id) external;
   function settleTransactionByMediator(uint256 _id, uint256 _buyerAmount, uint256 _sellerAmount) external;
   function provideTransactionFeedback(uint256 _id, uint8 _rating, bytes32 _comment) external;
+
+  /* ERC20 */
+  function approve(address _spender, uint256 _value) returns (bool success);
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  function balanceOf(address _owner) constant returns (uint256 balance);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  function function transfer(address _to, uint256 _value) returns (bool success);
 }
