10d85c10ff490af72521a6c8b8da6258958dd6e7
Ngan Pham <ngan@users.noreply.github.com>
2018-01-26 15:40:00 -0800

    Fix interface

--- contracts/InkProtocolInterface.sol
+++ contracts/InkProtocolInterface.sol
@@ -24,5 +24,5 @@ interface InkProtocolInterface {
   function allowance(address _owner, address _spender) constant returns (uint256 remaining);
   function balanceOf(address _owner) constant returns (uint256 balance);
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-  function function transfer(address _to, uint256 _value) returns (bool success);
+  function transfer(address _to, uint256 _value) returns (bool success);
 }
