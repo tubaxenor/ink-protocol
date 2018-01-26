364da48eec25672111e50c1df0b2f0b461a92f3b
Ngan Pham <ngan@users.noreply.github.com>
2018-01-26 15:29:37 -0800

    Update interface

--- contracts/InkProtocolInterface.sol
+++ contracts/InkProtocolInterface.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.11;
 
 interface InkProtocolInterface {
-  function authorize(address _agent) external;
-  function authorizedBy(address _account) public view returns (bool);
   function link(address _to) external;
-  function createTransaction(address _buyer, address _seller, uint256 _amount, bytes32 _metadata, address _policy, address _mediator) external;
+  function createTransaction(address _seller, uint256 _amount, bytes32 _metadata, address _policy, address _mediator) external;
+  function createTransactionWithOwner(address _seller, uint256 _amount, bytes32 _metadata, address _policy, address _mediator, address _owner) external;
   function revokeTransaction(uint256 _id) external;
   function acceptTransaction(uint256 _id) external;
   function confirmTransaction(uint256 _id) external;
@@ -18,5 +17,4 @@ interface InkProtocolInterface {
   function confirmTransactionByMediator(uint256 _id) external;
   function settleTransactionByMediator(uint256 _id, uint256 _buyerAmount, uint256 _sellerAmount) external;
   function provideTransactionFeedback(uint256 _id, uint8 _rating, bytes32 _comment) external;
-  function helloNgan() external;
 }
