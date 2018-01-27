55d792244f10c6b6bc7f2cb9688d5ddbbbe0b8f5
Ngan Pham <ngan@users.noreply.github.com>
2018-01-27 08:19:13 -0800

    Start rewriting tests

--- test/InkProtocol.js
+++ test/InkProtocol.js
@@ -1,28 +1,30 @@
 'use strict';
 
-let files = [
-  "./InkProtocol/gasAnalysis",
-  "./InkProtocol/authorize",
-  "./InkProtocol/authorizedBy",
-  "./InkProtocol/createTransaction",
-  "./InkProtocol/revokeTransaction",
-  "./InkProtocol/acceptTransaction",
-  "./InkProtocol/confirmTransaction",
-  "./InkProtocol/confirmTransactionAfterExpiry",
-  "./InkProtocol/refundTransaction",
-  "./InkProtocol/refundTransactionAfterExpiry",
-  "./InkProtocol/disputeTransaction",
-  "./InkProtocol/escalateDisputeToMediator",
-  "./InkProtocol/settleTransaction",
-  "./InkProtocol/refundTransactionByMediator",
-  "./InkProtocol/confirmTransactionByMediator",
-  "./InkProtocol/settleTransactionByMediator",
-  "./InkProtocol/provideTransactionFeedback"
-  "./InkProtocol/transfer"
-]
+// Commented out -- tests will be rewritten.
 
-for (let fileIndex in files) {
-  contract("InkProtocol", async (accounts) => {
-    require(files[fileIndex]).call(this, accounts)
-  })
-}
+// let files = [
+//   "./InkProtocol/gasAnalysis",
+//   "./InkProtocol/authorize",
+//   "./InkProtocol/authorizedBy",
+//   "./InkProtocol/createTransaction",
+//   "./InkProtocol/revokeTransaction",
+//   "./InkProtocol/acceptTransaction",
+//   "./InkProtocol/confirmTransaction",
+//   "./InkProtocol/confirmTransactionAfterExpiry",
+//   "./InkProtocol/refundTransaction",
+//   "./InkProtocol/refundTransactionAfterExpiry",
+//   "./InkProtocol/disputeTransaction",
+//   "./InkProtocol/escalateDisputeToMediator",
+//   "./InkProtocol/settleTransaction",
+//   "./InkProtocol/refundTransactionByMediator",
+//   "./InkProtocol/confirmTransactionByMediator",
+//   "./InkProtocol/settleTransactionByMediator",
+//   "./InkProtocol/provideTransactionFeedback"
+//   "./InkProtocol/transfer"
+// ]
+//
+// for (let fileIndex in files) {
+//   contract("InkProtocol", async (accounts) => {
+//     require(files[fileIndex]).call(this, accounts)
+//   })
+// }
