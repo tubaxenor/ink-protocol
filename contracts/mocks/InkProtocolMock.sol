4f2e5f11a705c5cd32b5f2c6f7ec25973780b934
Ngan Pham <ngan@users.noreply.github.com>
2018-01-29 10:24:03 -0800

    Remove unused methods from InkProtocolMock

--- contracts/mocks/InkProtocolMock.sol
+++ contracts/mocks/InkProtocolMock.sol
@@ -6,18 +6,4 @@ contract InkProtocolMock is InkProtocol {
   function InkProtocolMock() public {
     balances[msg.sender] = totalSupply_;
   }
-
-  function _updateTransactionState(uint _id, TransactionState _state) external {
-    transactions[_id].state = _state;
-  }
-
-  function _fetchTransaction(uint _id) external constant returns (address, address, address, TransactionState, uint) {
-    var transaction = transactions[_id];
-
-    return (transaction.buyer,
-            transaction.seller,
-            transaction.mediator,
-            transaction.state,
-            transaction.amount);
-  }
 }
