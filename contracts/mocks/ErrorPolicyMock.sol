ad6982c992eb6bbce9a635bbdfcca74c8649c65e
Jerry Chen <kraytruthg@gmail.com>
2018-01-31 02:40:50 +0800

    Add confirmTransactionAfterExpiry test (#11)

--- contracts/mocks/ErrorPolicyMock.sol
+++ contracts/mocks/ErrorPolicyMock.sol
@@ -4,17 +4,17 @@ import '../InkPolicy.sol';
 
 contract ErrorPolicyMock is InkPolicy {
   function transactionExpiry() external pure returns (uint32) {
-    assert(false);
+    revert();
     return 8 days;
   }
 
   function fulfillmentExpiry() external pure returns (uint32) {
-    assert(false);
+    revert();
     return 7 days;
   }
 
   function escalationExpiry() external pure returns (uint32) {
-    assert(false);
+    revert();
     return 6 days;
   }
 }
