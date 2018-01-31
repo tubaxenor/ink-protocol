abf980f4841df8736525124ef695bb645c25be19
Ngan Pham <ngan@users.noreply.github.com>
2018-01-31 14:00:07 -0800

    Update InkProtocol test

--- test/InkProtocol.js
+++ test/InkProtocol.js
@@ -8,26 +8,31 @@ contract("InkProtocol", (accounts) => {
     protocol = await InkProtocol.new()
   })
 
-  describe("#InkProtocol()", () => {
+  describe("#name()", () => {
     it("specifies the correct total supply", async () => {
       let totalSupply = await protocol.totalSupply()
-      let burnedSupply = await protocol.burnedSupply()
 
-      assert.equal(totalSupply.toNumber(), 500000000e18 - burnedSupply.toNumber())
+      assert.equal(totalSupply.toNumber(), 500000000e18)
     })
+  })
 
+  describe("#name()", () => {
     it("specifies the token name", async () => {
       let name = await protocol.name()
 
       assert.equal(name, "Ink Protocol")
     })
+  })
 
+  describe("#symbol()", () => {
     it("specifies the token symbol", async () => {
       let symbol = await protocol.symbol()
 
       assert.equal(symbol, "XNK")
     })
+  })
 
+  describe("#decimals()", () => {
     it("specifies the token decimals", async () => {
       let decimals = await protocol.decimals()
 
